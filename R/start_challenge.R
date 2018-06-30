9f4e458c1d2957bae2b1a28ae7136a6d554c63f0
Lucy McGowan <lucydagostino@gmail.com>
2018-06-30 12:48:17 -0500

    Add more explanation to start

--- R/start_challenge.R
+++ R/start_challenge.R
@@ -8,5 +8,18 @@ start_challenge <- function() {
 }
 
 .start_challenge <- function() {
+  if (start_question()) {
   matahari::dance_start()
+  }
+}
+
+start_question <- function() {
+  rstudioapi::showQuestion(title = "Ready?",
+                           message = glue::glue("  * We will be recording the commands you run in this session.\n  ",
+                                                "  * When you have finished, you will be prompted to copy the generated report ",
+                                                "and paste it into SurveyMonkey\n  ",
+                                                "  * You will then provide a short description of your data analysis and enter your final p-value obtained",
+                                                trim = FALSE),
+                           "Sounds good!",
+                           "cancel")
 }
